54267d710c2303f0310bd9bbb03af345577bc813
Błażej Domagała <petitoff@outlook.com>
2022-10-14 11:15:10 +0200

    added implementation for number keys

--- lista 2/zadanie 1/MainWindow.xaml.cs
+++ lista 2/zadanie 1/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace zadanie_1
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            AcButton.Click += ClearResult_Button;
+            NegativeButton.Click += NegativeConvert_Button;
+            PercentageButton.Click += PercentageCalculate_Button;
+            EqualButton.Click += equal_Button;
+        }
+
+        private void equal_Button(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PercentageCalculate_Button(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber /= 100;
+                ResultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeConvert_Button(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                ResultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void ClearResult_Button(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = "0";
+        }
+
+        private void NumberEvent_Button(object sender, RoutedEventArgs e)
+        {
+            var content = (sender as Button).Content;
+            if (ResultLabel.Content.ToString() == "0")
+            {
+                ResultLabel.Content = content as string;
+            }
+            else
+            {
+                ResultLabel.Content += content as string;
+
+            }
         }
     }
 }
