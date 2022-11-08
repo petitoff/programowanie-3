a7c9c2d1d275c7925e22684f74f90f9c476d078c
Błażej Domagała <petitoff@outlook.com>
2022-11-08 01:01:57 +0100

    added converters

---
+++ lista 2/Battleship/Battleship/Converter/IsEmptyColorButtonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Battleship.Converter
+{
+    internal class IsEmptyColorButtonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isEmpty = (bool)value;
+            if (isEmpty)
+            {
+                return "#fff";
+            }
+            else
+            {
+                return "#000";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
