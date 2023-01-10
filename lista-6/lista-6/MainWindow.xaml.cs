1534b0a72b34b86c9ef27afcd78f38761b1efdda
Błażej Domagała <petitoff@outlook.com>
2023-01-10 20:17:49 +0100

    add connection to database, display data and add data

--- lista-6/lista-6/MainWindow.xaml.cs
+++ lista-6/lista-6/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,57 @@ namespace lista_6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SqlConnection _con;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _con = new SqlConnection("Data Source=DESKTOP-IK2EFPM;Initial Catalog=TestSP_DB;User ID=salman;Password=Power!#2;MultipleActiveResultSets=true;");
+            _con.Open();
+            GetEmpList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // get all data from TextBox by x:Name and store in string
+            int empid = int.Parse(txtID.Text);
+            string name = txtName.Text;
+            string city = txtCity.Text;
+            float age = float.Parse(txtAge.Text);
+            string sex = "";
+
+            if (txtSex.Text == "Male" || txtSex.Text == "Female")
+            {
+                sex = txtSex.Text;
+            }
+
+            string contact = txtContact.Text;
+
+            // convert DateTime txtJoiningDate to sql DateTime format
+            DateTime joiningDate = DateTime.Parse(txtJoiningDate.Text);
+            string joiningDateSql = joiningDate.ToString("yyyy-MM-dd");
+
+
+            SqlCommand cmd = new SqlCommand($"exec InsertEmp_SP {empid}, '{name}', '{city}', {age}, '{sex}', '{joiningDateSql}', '{contact}'", _con);
+            // execute cmd
+
+            
+            cmd.ExecuteNonQuery();
+
+            MessageBox.Show("Stored Procedure Executed");
+
+            GetEmpList();
+        }
+
+        void GetEmpList()
+        {
+            SqlCommand cmd = new SqlCommand("exec ListEmp_SP", _con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            System.Data.DataTable dt = new System.Data.DataTable("Emp");
+            adapter.Fill(dt);
+            DataGridEmp.ItemsSource = dt.DefaultView;
         }
     }
 }
--- lista-6/lista-6/Properties/Settings.Designer.cs
+++ lista-6/lista-6/Properties/Settings.Designer.cs
@@ -8,22 +8,38 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace lista_6.Properties
-{
-
-
+namespace lista_6.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TestSP_DB;Integrated Security=True")]
+        public string TestSP_DBConnectionString {
+            get {
+                return ((string)(this["TestSP_DBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=TestSP_DB;User ID=salman;Password=Power!#2")]
+        public string TestSP_DBConnectionString1 {
+            get {
+                return ((string)(this["TestSP_DBConnectionString1"]));
             }
         }
     }
