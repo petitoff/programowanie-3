53df5b672e430568044be60551bd03330f204480
Błażej Domagała <petitoff@outlook.com>
2023-01-11 15:24:58 +0100

    setup Autofac with dependencies injection

---
+++ CarMechanic/CarMechanic.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using CarMechanic.UI.Data;
+using CarMechanic.UI.ViewModel;
+
+namespace CarMechanic.UI.Startup
+{
+    public class Bootstrapper
+    {
+        public IContainer BootStrap()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+
+            builder.RegisterType<CustomerDataService>().As<ICustomerDataService>();
+
+            return builder.Build();
+        }
+    }
+}
